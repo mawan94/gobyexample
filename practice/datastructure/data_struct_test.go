9de6291aae0e87cfafd7fa0ae2909b5897a37239
lidingkui <lidingkui7758521>
2021-06-17 14:29:53 +0800

    二分搜索

--- practice/datastructure/data_struct_test.go
+++ practice/datastructure/data_struct_test.go
@@ -25,4 +25,47 @@ func TestDataStruct(t *testing.T) {
 	list.print(func(e interface{}) {
 		fmt.Printf("%d ", e)
 	})
+	list.Swap(0, 3)
+	println()
+	list.print(func(e interface{}) {
+		fmt.Printf("%d ", e)
+	})
+	list.Swap(2, 1)
+	println()
+	list.print(func(e interface{}) {
+		fmt.Printf("%d ", e)
+	})
+
+	// ********************* BST **************************
+	bst := NewBinarySearchTree()
+	var a Integer = 4
+	var b Integer = 3
+	var c Integer = 2
+	var d Integer = 1
+
+	bst.add(a)
+	bst.add(b)
+	bst.add(c)
+	bst.add(d)
+
+	bst.remove(d)
+	bst.remove(a)
+
+	bst.bfs(func(e interface{}) {
+		fmt.Printf("%d ", e.(Integer))
+	})
+	bst.sortOrder(func(e interface{}) {
+		fmt.Printf("%d ", e.(Integer))
+	})
+}
+
+type Integer int
+
+func (self Integer) compareTo(other Comparable) int {
+	if self < other.(Integer) {
+		return -1
+	} else if self > other.(Integer) {
+		return 1
+	}
+	return 0
 }
--- practice/datastructure/model.go
+++ practice/datastructure/model.go
@@ -2,25 +2,26 @@ package datastructure
 
 import (
 	"fmt"
+	"sort"
 )
 
 /*
 	Model  -  FOR TEST
 */
 
-//单向链表
+// 单向链表节点
 type NodeList struct {
 	Val  interface{}
 	Next *NodeList
 }
 
-//双向链表
+// 双向链表节点
 type LinkedListNode struct {
 	Val        interface{}
 	Prev, Next *LinkedListNode
 }
 
-// 二叉树
+// 二叉树节点
 type TreeNode struct {
 	Val         interface{}
 	Left, Right *TreeNode
@@ -41,7 +42,12 @@ type Queue interface {
 	IsEmpty() bool
 }
 
-//TODO ARRAYLIST  LINKEDLIST       QUEUE  STACK  UPGRAY
+// 可比较接口
+type Comparable interface {
+	compareTo(other Comparable) int
+}
+
+// 双向链表实现
 type LinkedList struct {
 	dummyHead, dummyTail *LinkedListNode
 	size                 int
@@ -89,6 +95,25 @@ func (list *LinkedList) get(index int) *LinkedListNode {
 			curr = curr.Prev
 		}
 		return curr.Prev
+	}
+}
+
+func (list *LinkedList) set(index int, e interface{}) {
+	if index < 0 || index >= list.size {
+		panic(fmt.Sprintf("不合法的索引: %d", index))
+	}
+	if index <= list.size/2 {
+		curr := list.dummyHead.Next
+		for i := 0; i < index; i++ {
+			curr = curr.Next
+		}
+		curr.Val = e
+	} else {
+		curr := list.dummyTail.Prev
+		for i := list.size - 1; i > index; i-- {
+			curr = curr.Prev
+		}
+		curr.Val = e
 	}
 }
 
@@ -193,4 +218,179 @@ func (list *LinkedList) Dequeue() interface{} {
 
 func (list *LinkedList) IsEmpty() bool {
 	return list.size == 0
+}
+
+func (list *LinkedList) Len() int {
+	return list.size
+}
+
+func (list *LinkedList) Less(i, j int) bool {
+	iData, jData := list.get(i).Val, list.get(j).Val
+	ret := iData.(Comparable).compareTo(jData.(Comparable))
+	if ret > 0 {
+		return false
+	}
+	return true
+}
+
+func (list *LinkedList) Swap(i, j int) {
+	if i == j {
+		return
+	}
+	iData, jData := list.get(i).Val, list.get(j).Val
+	list.set(i, jData)
+	list.set(j, iData)
+}
+
+//func (list *LinkedList) Swap1(i, j int) {
+//	// 节点交换
+//	var node1 *LinkedListNode = list.dummyHead.Next
+//	var node2 *LinkedListNode = list.dummyHead.Next
+//	if i < 0 || i >= list.size {
+//		panic(fmt.Sprintf("不合法的索引: %d", i))
+//	}
+//	if j < 0 || j >= list.size {
+//		panic(fmt.Sprintf("不合法的索引: %d", j))
+//	}
+//	if i == j {
+//		return
+//	}
+//
+//	index1, index2 := i, j
+//	for index1 != 0 || index2 != 0 {
+//		if index1 != 0 {
+//			node1 = node1.Next
+//			index1--
+//		}
+//		if index2 != 0 {
+//			node2 = node2.Next
+//			index2--
+//		}
+//	}
+//	if i > j {
+//		node1, node2 = node2, node1
+//	}
+//
+//	node1Prev := node1.Prev
+//	node2Prev := node2.Prev
+//	// 断掉关联
+//	node1Prev.Next = nil
+//	node2Prev.Next = nil
+//
+//	node2.Next, node1.Next = node1.Next, node2.Next
+//
+//}
+
+func (list *LinkedList) sort() {
+	sort.Sort(list)
+}
+
+func (list *LinkedList) reverse() {
+	sort.Reverse(list)
+}
+
+// 二分搜索树
+// ***************** Binary Search Tree *****************
+type BinarySearchTree struct {
+	root *TreeNode
+	size int
+}
+
+func NewBinarySearchTree() *BinarySearchTree {
+	return &BinarySearchTree{}
+}
+
+func (bst *BinarySearchTree) add(e Comparable) {
+	var addFunc func(*TreeNode, Comparable) *TreeNode
+	addFunc = func(node *TreeNode, e Comparable) *TreeNode {
+		if node == nil {
+			bst.size++
+			if bst.root == nil {
+				bst.root = &TreeNode{Val: e}
+				return bst.root
+			}
+			return &TreeNode{Val: e}
+		}
+		if node.Val.(Comparable).compareTo(e) <= -1 {
+			node.Right = addFunc(node.Right, e)
+		} else if node.Val.(Comparable).compareTo(e) >= 1 {
+			node.Left = addFunc(node.Left, e)
+		}
+		return node
+	}
+	addFunc(bst.root, e)
+}
+
+func (bst *BinarySearchTree) remove(e Comparable) {
+	var removeFunc func(*TreeNode, Comparable) *TreeNode
+	removeFunc = func(node *TreeNode, comparable Comparable) *TreeNode {
+		if node == nil {
+			return nil
+		} else if node.Val.(Comparable).compareTo(e) <= -1 {
+			node.Right = removeFunc(node.Right, e)
+			return node
+		} else if node.Val.(Comparable).compareTo(e) >= 1 {
+			node.Left = removeFunc(node.Left, e)
+			return node
+		} else if node.Val.(Comparable).compareTo(e) == 0 {
+			bst.size--
+			if node.Left == nil {
+				if bst.root == node {
+					bst.root = node.Right
+				}
+				return node.Right
+			} else if node.Right == nil {
+				if bst.root == node {
+					bst.root = node.Left
+				}
+				return node.Left
+			}
+			replacement := node.Left
+			for replacement.Right != nil {
+				replacement = replacement.Right
+			}
+			replacement.Left = removeFunc(node.Left, replacement.Val.(Comparable))
+			replacement.Right = node.Right
+			if bst.root == node {
+				bst.root = replacement
+			}
+			return replacement
+		}
+		return node
+	}
+	removeFunc(bst.root, e)
+}
+
+func (bst *BinarySearchTree) bfs(printFunc func(e interface{})) {
+	println()
+	if bst.root == nil {
+		return
+	}
+	var queue Queue = NewLinkedList()
+	queue.Enqueue(bst.root)
+
+	for !queue.IsEmpty() {
+		node := queue.Dequeue().(*TreeNode)
+		printFunc(node.Val)
+		if node.Left != nil {
+			queue.Enqueue(node.Left)
+		}
+		if node.Right != nil {
+			queue.Enqueue(node.Right)
+		}
+	}
+}
+
+func (bst *BinarySearchTree) sortOrder(printFunc func(e interface{})) {
+	println()
+	var midOrder func(node *TreeNode)
+	midOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		midOrder(node.Left)
+		printFunc(node.Val)
+		midOrder(node.Right)
+	}
+	midOrder(bst.root)
 }
